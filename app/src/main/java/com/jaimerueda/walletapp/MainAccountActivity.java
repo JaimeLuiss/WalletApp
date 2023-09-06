dc0f3fcc0d9cd40b3facaaaed6ef2c741dc39170
Jaime Rueda <jrueda793@unab.edu.co>
2023-09-06 17:55:44 -0500

    Adelanto de la clase 06-09-2023

--- app/src/main/java/com/jaimerueda/walletapp/MainAccountActivity.java
+++ app/src/main/java/com/jaimerueda/walletapp/MainAccountActivity.java
@@ -2,13 +2,29 @@ package com.jaimerueda.walletapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainAccountActivity extends AppCompatActivity {
+
+    private ImageView ivBack;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_account);
+
+        ivBack = findViewById(R.id.iv_back);
+
+        ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainAccountActivity.this, MainActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
